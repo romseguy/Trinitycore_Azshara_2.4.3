64466e09f37c9cbafdaf55591beef82d32ce0c7b
Réda <Rda@Rda-PC>
2011-12-13 23:40:56 +0100

    REDA Fix Disarm exploit bug

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -6930,6 +6930,10 @@ void Player::_ApplyItemMods(Item *item, uint8 slot,bool apply)
 
     ApplyItemEquipSpell(item,apply);
     ApplyEnchantment(item, apply);
+	
+    // don't apply/remove mods if the weapon is disarmed
+     if (item->GetSlot() == EQUIPMENT_SLOT_MAINHAND && HasFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_DISARMED))
+        return;
 
     if (proto->Socket[0].Color)                              //only (un)equipping of items with sockets can influence metagems, so no need to waste time with normal items
         CorrectMetaGemEnchants(slot, apply);
