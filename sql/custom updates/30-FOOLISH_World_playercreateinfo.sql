6f99e7332830d4ca826750d7129afb566650b12d
Foolish <devnull@localhost>
2012-01-15 19:17:06 +0100

    FOOLISH Fix Horde shop spawn

---
+++ sql/custom updates/30-FOOLISH_World_playercreateinfo.sql
@@ -0,0 +1,8 @@
+update playercreateinfo set
+map =1,
+zone =1637,
+position_x = 2126.657227,
+position_y = -4732.455078,
+position_z = 50.196129,
+orientation = 2.535217
+where race in (2, 5, 6, 8, 10);
