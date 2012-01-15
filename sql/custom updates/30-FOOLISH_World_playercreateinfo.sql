7896919b62209e18b2715465fc3d80b9b746b669
Foolish <devnull@localhost>
2012-01-15 21:00:32 +0100

    FOOLISH Fix Alliance shop spawn

--- sql/custom updates/30-FOOLISH_World_playercreateinfo.sql
+++ sql/custom updates/30-FOOLISH_World_playercreateinfo.sql
@@ -6,3 +6,12 @@ position_y = -4732.455078,
 position_z = 50.196129,
 orientation = 2.535217
 where race in (2, 5, 6, 8, 10);
+
+update playercreateinfo set
+map =0,
+zone =1519,
+position_x = -8589.630,
+position_y = 800.852051,
+position_z = 106.514336,
+orientation = 3.801426
+where race in (1, 3, 4, 7, 11);
