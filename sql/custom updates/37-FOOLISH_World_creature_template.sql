e4dd8aca477b521602b93cf2e5ecb89219f1a840
Foolish <devnull@localhost>
2012-01-26 19:57:26 +0100

    FOOLISH Fix Professions specialities NPC

--- sql/custom updates/37-FOOLISH_World_creature_template.sql
+++ sql/custom updates/37-FOOLISH_World_creature_template.sql
@@ -1,2 +1,2 @@
 DELETE FROM `creature_template` WHERE `entry` = 400001;
-INSERT INTO `creature_template` VALUES (400001, 0, 0, 0, 17882, 17882, 17882, 17882, 'Blacksmithing and Engineering', 'Specialities', NULL, 0, 70, 70, 10000, 10000, 0, 0, 0, 35, 35, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 0, 1, 0, 0, 0, 'npc_sub_profession');
+INSERT INTO `creature_template` VALUES (400001, 0, 0, 0, 17882, 17882, 17882, 17882, 'Professions Specialities', '', NULL, 0, 70, 70, 10000, 10000, 0, 0, 0, 35, 35, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 0, 1, 0, 0, 0, 'npc_sub_profession');
--- src/scripts/Custom/npc_sub_profession.cpp
+++ src/scripts/Custom/npc_sub_profession.cpp
@@ -9,6 +9,16 @@
 #define HAMMER   17040
 #define AXE      17041
 
+#define LEATHERWORKING_SPELL   32549
+#define DRAGON 10656
+#define ELEM   10658
+#define TRIBAL 10660
+
+#define TAILORING_SPELL        26790
+#define SPELLFIRE  26797
+#define MOONCLOTH  26798
+#define SHADOWEAVE 26801
+
 #define WEAPONS  9787
 #define ARMORS   9788
 
@@ -33,32 +43,76 @@ bool GossipSelect_npc_sub_profession(Player *player, Creature *_Creature, uint32
         return true;
     }
 
+    if ((opt == 6 || opt == 7 || opt == 8) && !player->HasSpell(LEATHERWORKING_SPELL))
+    {
+        player->GetSession()->SendNotification("You have to learn leatherworking first.");
+        player->PlayerTalkClass->CloseGossip();
+        return true;
+    }
+
+    if ((opt == 9 || opt == 10 || opt == 11) && !player->HasSpell(LEATHERWORKING_SPELL))
+    {
+        player->GetSession()->SendNotification("You have to learn tailoring first.");
+        player->PlayerTalkClass->CloseGossip();
+        return true;
+    }
+
     switch (opt)
     {
         case 1:
-            SpellId = 20222;
-            SpellIdx = 20219;
+            SpellId = GOBLIN;
+            SpellIdx = GNOMISH;
             SpellIdxx = 0;
             break;
         case 2:
-            SpellId = 20219;
-            SpellIdx = 20222;
+            SpellId = GNOMISH;
+            SpellIdx = GOBLIN;
             SpellIdxx = 0;
             break;
         case 3:
-            SpellId = 17039;
-            SpellIdx = 17040;
-            SpellIdxx = 17041;
+            SpellId = SWORD;
+            SpellIdx = HAMMER;
+            SpellIdxx = AXE;
             break;
         case 4:
-            SpellId = 17040;
-            SpellIdx = 17039;
+            SpellId = HAMMER;
+            SpellIdx = SWORD;
             SpellIdxx = 17041;
             break;
         case 5:
-            SpellId = 17041;
-            SpellIdx = 17039;
-            SpellIdxx = 17040;
+            SpellId = AXE;
+            SpellIdx = SWORD;
+            SpellIdxx = HAMMER;
+            break;
+        case 6:
+            SpellId = DRAGON;
+            SpellIdx = ELEM;
+            SpellIdxx = TRIBAL;
+            break;
+        case 7:
+            SpellId = ELEM;
+            SpellIdx = DRAGON;
+            SpellIdxx = TRIBAL;
+            break;
+        case 8:
+            SpellId = TRIBAL;
+            SpellIdx = ELEM;
+            SpellIdxx = DRAGON;
+            break;
+        case 9:
+            SpellId = SPELLFIRE;
+            SpellIdx = MOONCLOTH;
+            SpellIdxx = SHADOWEAVE;
+            break;
+        case 10:
+            SpellId = MOONCLOTH;
+            SpellIdx = SPELLFIRE;
+            SpellIdxx = SHADOWEAVE;
+            break;
+        case 11:
+            SpellId = SHADOWEAVE;
+            SpellIdx = SPELLFIRE;
+            SpellIdxx = MOONCLOTH;
             break;
     }
 
@@ -90,6 +144,12 @@ bool GossipHello_npc_sub_profession(Player *player, Creature *_Creature)
     player->ADD_GOSSIP_ITEM_EXTENDED(0, "Master swordsmith",         GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+3, WARN_RESET, 0, false);
     player->ADD_GOSSIP_ITEM_EXTENDED(0, "Master hammersmith",        GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+4, WARN_RESET, 0, false);
     player->ADD_GOSSIP_ITEM_EXTENDED(0, "Master axesmith",           GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+5, WARN_RESET, 0, false);
+    player->ADD_GOSSIP_ITEM_EXTENDED(0, "Dragonscale leatherworking",GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+6, WARN_RESET, 0, false);
+    player->ADD_GOSSIP_ITEM_EXTENDED(0, "Elemental leatherworking",  GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+7, WARN_RESET, 0, false);
+    player->ADD_GOSSIP_ITEM_EXTENDED(0, "Tribal leatherworking",     GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+8, WARN_RESET, 0, false);
+    player->ADD_GOSSIP_ITEM_EXTENDED(0, "Spellfire tailoring",       GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+9, WARN_RESET, 0, false);
+    player->ADD_GOSSIP_ITEM_EXTENDED(0, "Mooncloth tailoring",       GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+10, WARN_RESET, 0, false);
+    player->ADD_GOSSIP_ITEM_EXTENDED(0, "Shadoweave tailoring",      GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+11, WARN_RESET, 0, false);
     player->PlayerTalkClass->SendGossipMenu(2, _Creature->GetGUID());
 
     return true;
